5cab382a02102bec4b3db994ae53b7b113af7661
RodrigoCast <rodrigocastp05@gmail.com>
2023-05-24 19:02:48 -0500

    Task Script Hello World

---
+++ index.js
@@ -0,0 +1,18 @@
+//Este es el mensaje de bienvenida
+console.log("Esta es la puerta de entrada a el proyecto");
+
+//Esta es la parte intermedia de nuestro proyecto
+//En esta parte realizamos los cheuqeos necesarios
+//Por ultimo vamos a dar la ultima linea de saludo
+
+/*
+    Esta es la parte intermedia de nuestro proyecto
+    En esta parte realizamos los cheuqeos necesarios
+    Por ultimo vamos a dar la ultima linea de saludo
+    Esto tambien esta comentado
+
+*/
+
+
+//Este va a ser el mensaje de despedida
+console.log("Adiós")
